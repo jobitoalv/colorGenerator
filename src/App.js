0fd83546ae523d81fffc5cfd25599fa8b3c3f15f
Job Alvarez <jobitoalv@gmail.com>
2023-01-10 14:09:06 +1000

    create new function for SingleColor.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,52 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  const [color, setColor] = useState('');
-  const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values('#f15025').all(10));
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
-      let color = new Values(color).all(10)
+      let colors = new Values(color).all(10)
       setList(colors)
-    } catch (error){
+    } catch (error) {
       setError(true)
       console.log(error)
     }
   }
 
-
-  return <h2>color generator project</h2>
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
