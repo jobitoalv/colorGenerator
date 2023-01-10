dfd67186fd34b723b4dd7f563ac98b09c8a033bc
Job Alvarez <jobitoalv@gmail.com>
2023-01-10 14:07:09 +1000

    add default values for color

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
       let color = new Values(color).all(10)
-      setList(color)
+      setList(colors)
     } catch (error){
       setError(true)
       console.log(error)
